fbaa8d7c64c0d90a84c44d7e2bf2e244ac136518
danielislas3 <danielislas3@live.com>
2020-10-12 23:59:51 -0500

    docs

---
+++ docs/precache-manifest.b3241658eaa60d75ab4a074bc0e75920.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037b9a28b6df2267447ac6a8682dfb73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e88c1070e70695762a01",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "3de092551b0c77e78300",
+    "url": "/static/css/main.ef00b9d8.chunk.css"
+  },
+  {
+    "revision": "e88c1070e70695762a01",
+    "url": "/static/js/2.dae3937b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dae3937b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de092551b0c77e78300",
+    "url": "/static/js/main.acc7ff80.chunk.js"
+  },
+  {
+    "revision": "28b2448ea3201b2f979f",
+    "url": "/static/js/runtime-main.09460a5c.js"
+  }
+]);
