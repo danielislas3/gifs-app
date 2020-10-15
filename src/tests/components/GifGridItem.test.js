4b74e6243c62d5074fa2ee3b188d4cc66e2d030d
danielislas3 <danielislas3@live.com>
2020-10-15 12:36:21 -0500

    tests en div y css

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -1,16 +1,34 @@
-import '@testing-library/jest-dom'
-import React from 'react'
-import {shallow} from 'enzyme'
-import { GifGridItem } from '../../components/GifGridItem'
+import "@testing-library/jest-dom";
+import React from "react";
+import { shallow } from "enzyme";
+import { GifGridItem } from "../../components/GifGridItem";
 
-describe('Pruebas en <GifGridItem></GifGridItem>', () => {
-  const title ='Un titulo'
-  const url ='http//'
-  test('Debe mostrar el copmponenete correctamnte', () => {
-    
-const wrapper = shallow(<GifGridItem title={title} url={url}/>) 
+describe("Pruebas en <GifGridItem></GifGridItem>", () => {
 
-   expect(wrapper).toMatchSnapshot()
+  const title = "Un titulo";
+  const url = "http//";
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
+
+  test("Debe mostrar el copmponenete correctamnte", () => {
+
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Debe de tener un parrafo con titulo ', () => {
+    const p=wrapper.find('p')
+    expect(p.text().trim()).toBe(title)
   })
   
-})
+  test('Debe de tener la imagen igual al url y alt de los props ', () => {
+    const img= wrapper.find('img')
+    expect(img.props().src).toBe(url)
+  })
+  
+  test('Debe de tener animacion css ', () => {
+    const div= wrapper.find('div')
+    const className= div.prop('className')
+    expect(className.includes('animate_fadeIn')).toBe(false)
+  })
+  
+});
