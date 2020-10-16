c348f81c2358e62fe5648f4098ad4e65bf7e44e2
danielislas3 <danielislas3@live.com>
2020-10-16 09:27:06 -0500

    test en lista de items

--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -35,5 +35,9 @@ describe("Pruebas en <GifGrid/>", () => {
     const wrapper = shallow(<GifGrid category={category} />);
     
     expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('p').exists()).toBe(false);
+    expect(wrapper.find('GifGridItem').length).toBe(gifs.length)
+
+
   });
 });
