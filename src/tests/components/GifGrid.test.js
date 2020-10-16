663eef855164eef1659790e30ef2e7a480933050
danielislas3 <danielislas3@live.com>
2020-10-16 09:20:22 -0500

    test custom hook

---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,39 @@
+import "@testing-library/jest-dom";
+import React from "react";
+import { shallow } from "enzyme";
+import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+jest.mock("../../hooks/useFetchGifs");
+
+describe("Pruebas en <GifGrid/>", () => {
+
+  const category = "Pepe";
+  
+  test("Debe mostrar el copmponenete ", () => {
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
+
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("debe de mostrar items cuando se cargan imagenes", () => {
+
+    const gifs=[{
+      id:'abc',
+      url:'https://giphy.com/embed/HdVtsppIpBigM',
+      title:'gift1'
+    }]
+
+    useFetchGifs.mockReturnValue({
+      data: gifs,
+      loading: false,
+    });
+
+    const wrapper = shallow(<GifGrid category={category} />);
+    
+    expect(wrapper).toMatchSnapshot();
+  });
+});
